504574137594a018358b29d7cc7ca75a0a6b7e71
Gayla Cloud <gaylacloud@Gaylas-MacBook-Pro.local>
2020-12-02 19:15:36 -0600

    https://github.com/gcloud11/Rentown.git

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import logo from './Images/Logo.png';
+// import Logo from '';
+import Login from './components/Login';
 import './App.css';
 
 
@@ -6,7 +7,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+       {/* <Login /> */}
     </div>
   );
 }
