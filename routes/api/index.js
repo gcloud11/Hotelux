2654ef0207e856ca6969d4df80400fa2f77a21b9
Cole Starnes <colestarnes1@gmail.com>
2020-12-02 21:18:08 -0600

    api start

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,33 @@
+import axios from "axios";
+
 const router = require("express").Router();
 
 //Requiring the js page to the right root routes
 router.use("/user", require("./user.js"));
 router.use("/auth", require("./auth.js"));
+
+
+
+const options = {
+  method: 'POST',
+  url: 'https://zillowdimashirokovv1.p.rapidapi.com/getDeepSearchResults.htm',
+  headers: {
+    'content-type': 'application/x-www-form-urlencoded',
+    'x-rapidapi-key': '3120aeda24mshcbd73afb8073f50p141138jsnb860a762d82d',
+    'x-rapidapi-host': 'ZillowdimashirokovV1.p.rapidapi.com'
+  },
+  data: {
+    citystatezip: 'undefined',
+    'zws-Id': '3120aeda24mshcbd73afb8073f50p141138jsnb860a762d82d',
+    rentzestimate: 'true,false',
+    address: 'undefined'
+  }
+};
+
+axios.request(options).then(function (response) {
+	console.log(response.data);
+}).catch(function (error) {
+	console.error(error);
+});
 
 module.exports = router;
