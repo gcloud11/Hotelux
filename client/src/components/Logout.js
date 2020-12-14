dfcbb24e26c6e853705f01fb5647bb315a1ca02a
Sarah Seagraves <67175494+seagraving@users.noreply.github.com>
2020-12-14 01:26:36 -0600

    fixing logout

--- client/src/components/Logout.js
+++ client/src/components/Logout.js
@@ -1,12 +1,23 @@
 import { GoogleLogout } from 'react-google-login';
+import React from 'react';
+
+const clientId = "658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com"
 
 function Logout() {
-    return (<GoogleLogout
-        clientId="658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com"
-        buttonText="Logout"
-        onLogoutSuccess="/login"
-      >
-      </GoogleLogout>)
+    const onSuccess = () => {
+        alert('Logout made successfully!');
+    }
+
+    return (
+        <div>
+            <GoogleLogout
+                clientId= {clientId}
+                buttonText="Logout"
+                onLogoutSuccess={onSuccess}
+            >
+            </GoogleLogout>
+        </div>
+    )
 }
 
 export default Logout;
